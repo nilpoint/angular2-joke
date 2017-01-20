96ab4d6552c49eaadf5d62eb74e2923b70d000ab
nilpoint <nilpoint@hotmail.com>
2017-01-20 20:47:09 +0800

    Change 'tell me' button style

--- app/app.joke.component.ts
+++ app/app.joke.component.ts
@@ -8,7 +8,7 @@ import { Joke } from './Joke';
   <div class="card card-block">
     <h4 class="card-title">{{joke.setup}}</h4>
     <p class="card-text" [hidden]="joke.hide">{{joke.punchline}}</p>
-    <a class="btn btn-primary" (click)="joke.toggle()">Tell Me!</a>
+    <a class="btn btn-info" (click)="joke.toggle()">Tell Me!</a>
     <a class="btn btn-danger" (click)="removeJoke(joke)">Delete</a>
   </div>
   `
